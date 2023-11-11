3eccfa80b31549f9fa2791d5d65bc122a2b6aea6
Fix1t <gbiel1117@gmail.com>
2023-11-11 16:58:24 +0100

    db: finalise tables and fix user routs to not throw err

--- database.sql
+++ database.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE iot;
-
-CREATE TABLE User (
+CREATE TABLE Users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) NOT NULL UNIQUE,
     mail VARCHAR(255) NOT NULL UNIQUE,
@@ -12,84 +10,84 @@ CREATE TABLE User (
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE System (
+CREATE TABLE Systems (
     id INT PRIMARY KEY AUTO_INCREMENT,
     owner_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (owner_id) REFERENCES User(id)
+    FOREIGN KEY (owner_id) REFERENCES Users(id)
 );
 
-CREATE TABLE SystemUser (
+CREATE TABLE SystemUsers (
     system_id INT NOT NULL,
     user_id INT NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (system_id, user_id),
-    FOREIGN KEY (system_id) REFERENCES System(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
+    FOREIGN KEY (system_id) REFERENCES Systems(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE SystemUserRequest (
+CREATE TABLE SystemUserRequests (
     id INT PRIMARY KEY AUTO_INCREMENT,
     system_id INT NOT NULL,
     user_id INT NOT NULL,
     status ENUM('pending', 'accepted', 'rejected') NOT NULL,
     message TEXT,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (system_id) REFERENCES System(id) ON DELETE CASCADE,
-    FOREIGN KEY (user_id) REFERENCES User(id) ON DELETE CASCADE
+    FOREIGN KEY (system_id) REFERENCES Systems(id) ON DELETE CASCADE,
+    FOREIGN KEY (user_id) REFERENCES Users(id) ON DELETE CASCADE
 );
 
-CREATE TABLE Type (
+CREATE TABLE Types (
     id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE Device (
+CREATE TABLE Devices (
     id INT PRIMARY KEY AUTO_INCREMENT,
     type_id INT NOT NULL,
     name VARCHAR(255) NOT NULL UNIQUE,
     description TEXT,
     user_alias VARCHAR(255),
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (type_id) REFERENCES Type(id) ON DELETE CASCADE
+    FOREIGN KEY (type_id) REFERENCES Types(id) ON DELETE CASCADE
 );
 
-CREATE TABLE SystemDevice (
+CREATE TABLE SystemDevices (
     device_id INT NOT NULL,
     system_id INT NOT NULL,
     created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (device_id, system_id),
-    FOREIGN KEY (device_id) REFERENCES Device(id) ON DELETE CASCADE,
-    FOREIGN KEY (system_id) REFERENCES System(id) ON DELETE CASCADE
+    FOREIGN KEY (device_id) REFERENCES Devices(id) ON DELETE CASCADE,
+    FOREIGN KEY (system_id) REFERENCES Systems(id) ON DELETE CASCADE
 );
 
 -- Template table - what parameters a device type has
-CREATE TABLE Parameter (
+CREATE TABLE Parameters (
     id INT PRIMARY KEY AUTO_INCREMENT,
     type_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
-    FOREIGN KEY (type_id) REFERENCES Type(id) ON DELETE CASCADE
+    FOREIGN KEY (type_id) REFERENCES Types(id) ON DELETE CASCADE
 );
 
 -- Table that stores the value of a parameter of a device at a certain time
-CREATE TABLE DeviceParameter (
+CREATE TABLE DeviceParameters (
     device_id INT NOT NULL,
     parameter_id INT NOT NULL,
     value DECIMAL(10,2) NOT NULL,
     recorded_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (device_id, parameter_id, recorded_at),
-    FOREIGN KEY (device_id) REFERENCES Device(id) ON DELETE CASCADE,
-    FOREIGN KEY (parameter_id) REFERENCES Parameter(id) ON DELETE CASCADE
+    FOREIGN KEY (device_id) REFERENCES Devices(id) ON DELETE CASCADE,
+    FOREIGN KEY (parameter_id) REFERENCES Parameters(id) ON DELETE CASCADE
 );
 
-CREATE TABLE KPI (
+CREATE TABLE KPIs (
     id INT PRIMARY KEY AUTO_INCREMENT,
     system_id INT NOT NULL,
     parameter_id INT NOT NULL,
     threshold DECIMAL(10,2) NOT NULL,
     operation ENUM('greater', 'less', 'equal', 'not_equal') NOT NULL,
-    FOREIGN KEY (system_id) REFERENCES System(id) ON DELETE CASCADE,
-    FOREIGN KEY (parameter_id) REFERENCES Parameter(id) ON DELETE CASCADE
+    FOREIGN KEY (system_id) REFERENCES Systems(id) ON DELETE CASCADE,
+    FOREIGN KEY (parameter_id) REFERENCES Parameters(id) ON DELETE CASCADE
 );

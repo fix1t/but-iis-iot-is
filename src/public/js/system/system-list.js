141d614b45770e4156c605dc720ce75cfbfac185
adamgabry <adam.gabrys@seznam.cz>
2023-11-21 23:57:50 +0100

    added SystemList to login page, redirecting to Systems after login

--- src/public/js/system/system-list.js
+++ src/public/js/system/system-list.js
@@ -5,25 +5,26 @@ fetch('/api/systems/get')
         const systemsTableBody = document.getElementById('systemsTableBody');
         data.forEach(system => {
             const row = document.createElement('tr');
-                        //<td>${system.owner_id}</td>
-                        //<td>${new Date(system.created).toLocaleString()}</td>
-            row.innerHTML = `
-                        <td>${system.id}</td>
-                        <td>${system.name}</td>
-                        <td>${system.owner_name}</td>
-                        <td>${system.description}</td>
-                        <td>
-                        <button class="btn btn-primary btn-sm" onclick="showSystemDetail(${system.id})">
-                            <i class="fas fa-info" style="padding: 0.25rem;"></i>
+            let buttonHTML = '';
+            if (window.location.pathname !== '/login') {
+                buttonHTML = `
+                    <button class="btn btn-primary btn-sm" onclick="showSystemDetail(${system.id})">
+                        <i class="fas fa-info" style="padding: 0.25rem;"></i>
                     </button>
-                    </td>
-                    `;
+                `;
+            }
+            row.innerHTML = `
+                <td>${system.id}</td>
+                <td>${system.name}</td>
+                <td>${system.owner_name}</td>
+                <td>${system.description}</td>
+                <td>${buttonHTML}</td>
+            `;
             row.id = `systemRow_${system.id}`;
             systemsTableBody.appendChild(row);
         });
     })
     .catch(error => console.error('Error fetching data:', error));
-
 // Fetch data from the server and print user system table
 fetch('/api/systems/in')
 .then(response => response.json())

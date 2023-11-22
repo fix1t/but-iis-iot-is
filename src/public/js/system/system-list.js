c314d66f6319a7d264e441fe727e1904e25b0ddd
adamgabry <adam.gabrys@seznam.cz>
2023-11-22 21:07:56 +0100

    showing my device lists, SystemList is Systems where  user is NOT in, everything scrollable

--- src/public/js/system/system-list.js
+++ src/public/js/system/system-list.js
@@ -1,57 +1,57 @@
-// Fetch data from the server and print system table
-fetch('/api/systems/get')
-    .then(response => response.json())
-    .then(data => {
-        const systemsTableBody = document.getElementById('systemsTableBody');
-        data.forEach(system => {
-            const row = document.createElement('tr');
-            let buttonHTML = '';
-            if (window.location.pathname !== '/login') {
-                buttonHTML = `
-                    <button class="btn btn-primary btn-sm" onclick="showSystemDetail(${system.id})">
-                        <i class="fas fa-info" style="padding: 0.25rem;"></i>
-                    </button>
-                `;
-            }
-            row.innerHTML = `
-                <td>${system.id}</td>
-                <td>${system.name}</td>
-                <td>${system.owner_name}</td>
-                <td>${system.description}</td>
-                <td>${buttonHTML}</td>
-            `;
-            row.id = `systemRow_${system.id}`;
-            systemsTableBody.appendChild(row);
-        });
-    })
-    .catch(error => console.error('Error fetching data:', error));
 // Fetch data from the server and print user system table
 fetch('/api/systems/in')
 .then(response => response.json())
-.then(data => {
-    const userSystemsTableBody = document.getElementById('userSystemsTableBody');
-    data.forEach(system => {
-        const row = document.createElement('tr');
-                    //<td>${system.owner_id}</td>
-                    //<td>${new Date(system.created).toLocaleString()}</td>
-        row.innerHTML = `
-                    <td>${system.id}</td>
-                    <td>${system.name}</td>
-                    <td>${system.owner_name}</td>
-                    <td>${system.description}</td>
-                    <td>
-                    <button class="btn btn-primary btn-sm" onclick="showSystemDetail(${system.id})">
-                        <i class="fas fa-info" style="padding: 0.25rem;"></i>
-                    </button>
-                    </td>
-                `;
-        row.id = `userSystemRow_${system.id}`;
-        userSystemsTableBody.appendChild(row);
-    });
+.then(userSystems => {
+	const userSystemsTableBody = document.getElementById('userSystemsTableBody');
+	userSystems.forEach(system => {
+		const row = document.createElement('tr');
+		row.innerHTML = `
+			<td>${system.id}</td>
+			<td>${system.name}</td>
+			<td>${system.owner_name}</td>
+			<td>${system.description}</td>
+			<td>
+				<button class="btn btn-primary btn-sm" onclick="showSystemDetail(${system.id})">
+					<i class="fas fa-info" style="padding: 0.25rem;"></i>
+				</button>
+			</td>
+		`;
+		row.id = `userSystemRow_${system.id}`;
+		userSystemsTableBody.appendChild(row);
+	});
+
+// Fetch data from the server and print system table
+fetch('/api/systems/get')
+	.then(response => response.json())
+	.then(allSystems => {
+		const systemsTableBody = document.getElementById('systemsTableBody');
+		allSystems.forEach(system => {
+			// Check if the system is not in userSystems
+			if (!userSystems.find(userSystem => userSystem.id === system.id)) {
+				const row = document.createElement('tr');
+				let buttonHTML = '';
+				if (window.location.pathname !== '/login') {
+					buttonHTML = `
+						<button class="btn btn-primary btn-sm" onclick="showSystemDetail(${system.id})">
+							<i class="fas fa-info" style="padding: 0.25rem;"></i>
+						</button>
+					`;
+				}
+				row.innerHTML = `
+					<td>${system.id}</td>
+					<td>${system.name}</td>
+					<td>${system.owner_name}</td>
+					<td>${system.description}</td>
+					<td>${buttonHTML}</td>
+				`;
+				row.id = `systemRow_${system.id}`;
+				systemsTableBody.appendChild(row);
+			}
+		});
+	})
+	.catch(error => console.error('Error fetching data:', error));
 })
 .catch(error => console.error('Error fetching data:', error));
-
-
 
 function deleteSystem(systemId) {
     // Make a DELETE request to delete the system with the specified systemId

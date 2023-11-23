9b7cd8d5910b942ec1147c3d3fb0811322580048
Fix1t <gbiel1117@gmail.com>
2023-11-23 20:12:46 +0100

    added common logger util

---
+++ src/utils/logger.js
@@ -0,0 +1,11 @@
+export const INFO = (data) => {
+	console.log("INFO >> " + data);
+}
+
+export const WARN = (data) => {
+	console.log("WARN >> " + data);
+}
+
+export const ERROR = (data) => {
+	console.log("ERROR >> " + data);
+}

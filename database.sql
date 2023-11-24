701cd31e56c2af6c80c7770f077e13b97ed32b17
Fix1t <gbiel1117@gmail.com>
2023-11-24 16:50:16 +0100

    updated DB schema to include unit_name

--- database.sql
+++ database.sql
@@ -74,6 +74,7 @@ CREATE TABLE Parameters (
     id INT PRIMARY KEY AUTO_INCREMENT,
     type_id INT NOT NULL,
     name VARCHAR(255) NOT NULL,
+    unit_name VARCHAR(255) NOT NULL,
     FOREIGN KEY (type_id) REFERENCES Types(id) ON DELETE CASCADE
 );
 

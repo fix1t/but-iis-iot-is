8efb7395dca30f6cff5f8358e3e0f3eb7932c78c
Mikynek <mikysek.jakub@gmail.com>
2023-11-24 11:15:36 +0100

    reload on action

--- src/routes/files-routes.js
+++ src/routes/files-routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { home, login, register, userList, userEdit, systemUsers, systemList, systemEdit, systemCreate, systemDetail, systemRequests, deviceDetail, deviceCreate } from '../controllers/files-controller.js';
+import { home, login, register, userList, userEdit, systemList, systemEdit, systemCreate, systemDetail, systemRequests, deviceDetail, deviceCreate } from '../controllers/files-controller.js';
 import { verifyToken, redirectIfAuthenticated, continueIfUserIsInSystem } from '../utils/auth.js';
 
 const router = express.Router();
@@ -12,7 +12,6 @@ router.get('/users/edit', verifyToken, userEdit);
 router.get('/systems', verifyToken, systemList);
 router.get('/systems/edit/:id', verifyToken, systemEdit);
 router.get('/systems/create', verifyToken, systemCreate);
-router.get('/systems/util', verifyToken, systemUsers);
 router.get('/systems-requests', verifyToken, systemRequests);
 router.get('/systems/detail/:id', verifyToken, systemDetail);
 router.get('/device/detail/:id', verifyToken, deviceDetail);

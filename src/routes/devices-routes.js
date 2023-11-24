33e53f3b92ca58ddc8e442aa21ac78da63e34257
Fix1t <gbiel1117@gmail.com>
2023-11-24 20:15:22 +0100

    added get all  kpis EP

--- src/routes/devices-routes.js
+++ src/routes/devices-routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { createDevice, getMyDevices, getDeviceById, getAllTypes } from '../controllers/devices-controller.js';
-import { getParameterById, getAllValuesByParameterIdAndDeviceId, getAllParametersAndValuesByDeviceId } from '../controllers/parameters-controller.js';
+import { getParameterById, getAllValuesByParameterIdAndDeviceId, getAllParametersAndValuesByDeviceId, getAllKpisByParameterIdAndDeviceId } from '../controllers/parameters-controller.js';
 import { verifyToken, continueIfUserIsInSystem } from '../utils/auth.js';
 
 const router = express.Router();
@@ -12,6 +12,7 @@ router.get('/:device_id', verifyToken, getDeviceById);
 router.get('/:device_id/parameters', verifyToken, getAllParametersAndValuesByDeviceId);
 router.get('/:device_id/parameters/:parameter_id', verifyToken, getParameterById)
 router.get('/:device_id/parameters/:parameter_id/data', verifyToken, getAllValuesByParameterIdAndDeviceId);
+router.get('/:device_id/parameters/:parameter_id/kpis', verifyToken, getAllKpisByParameterIdAndDeviceId);
 //post
 router.post('/create/:system_id', verifyToken, continueIfUserIsInSystem, createDevice);
 router.post('/create', verifyToken, createDevice);

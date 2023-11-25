b53d011aa30aeab669c88664174967222767f415
adamgabry <adam.gabrys@seznam.cz>
2023-11-25 18:14:41 +0100

    showing button to create type only to ADMIN

--- src/public/js/type/type-list.js
+++ src/public/js/type/type-list.js
@@ -1,3 +1,7 @@
+let userId;
+let isAdmin;
+
+
 fetch('/api/types/get')
     .then(response => response.json())
     .then(types => {
@@ -14,3 +18,20 @@ fetch('/api/types/get')
         });
     })
     .catch(error => console.error('Error:', error));
+
+async function checkUserAdminStatus() {
+	// Get logged User
+	const user = await fetch(`/api/users/me`);
+	const userResponse = await user.json();
+	const userId = userResponse.id;
+	const isAdmin = userResponse.is_admin;
+
+	const createTypeButton = document.getElementById('createTypeButton');
+	if (isAdmin) {
+		// hide the create type button for non-admin users
+		createTypeButton.classList.remove('d-none');
+	}
+}
+
+// Call the function when the page loads
+window.onload = checkUserAdminStatus;

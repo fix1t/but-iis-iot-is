c839257a255909b96664084e7e5d4ca1240d4722
adamgabry <adam.gabrys@seznam.cz>
2023-11-25 12:09:25 +0100

    adding Type

--- src/routes/types-routes.js
+++ src/routes/types-routes.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import { verifyToken, continueIfUserIsInSystem } from '../utils/auth.js';
-import { getAllTypesWithParameters } from '../controllers/type-controller.js';
+import { getAllTypesWithParameters, createType } from '../controllers/type-controller.js';
 
 const router = express.Router();
 
 //get
 router.get('/get', verifyToken, getAllTypesWithParameters);
+//post
+router.post('/create', verifyToken, createType);
 
 
 export default router;

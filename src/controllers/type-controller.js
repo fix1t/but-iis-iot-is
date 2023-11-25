2f75a3dc94c1a5e27336b0f24549db3c656f9612
adamgabry <adam.gabrys@seznam.cz>
2023-11-25 12:13:35 +0100

    deleted unused fctions

--- src/controllers/type-controller.js
+++ src/controllers/type-controller.js
@@ -1,5 +1,4 @@
 import Type from '../models/type-model.js';
-import Parameter from '../models/parameter-model.js';
 import { INFO } from '../utils/logger.js';
 
 
@@ -19,73 +18,11 @@ export const createType = async (req, res) => {
     }
 };
 
-export const addParameterToType = async (req, res) => {
-    const { typeId, paramId } = req.params;
-
-    try {
-        // Find the type and parameter in the database
-        const type = await Type.findById(typeId);
-        const parameter = await Parameter.findById(paramId);
-
-        // Add the parameter to the type
-        type.parameters.push(parameter);
-
-        // Save the updated type to the database
-        const updatedType = await type.save();
-
-        // Respond with the updated type
-        res.status(200).json(updatedType);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-};
 
 export const getAllTypesWithParameters = async (req, res) => {
     try {
         const types = await Type.findAll();
         res.status(200).json(types);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-};
-
-
-export const addParametersToType = async (req, res) => {
-    const { typeId } = req.params;
-    const { parameterIds } = req.body;
-
-    try {
-        // Find the type in the database
-        const type = await Type.findById(typeId);
-
-        // Find the parameters in the database and add them to the type
-        const parameterPromises = parameterIds.map(async paramId => {
-            const parameter = await Parameter.findById(paramId);
-            parameter.type_id = type.id;
-            return parameter.save();
-        });
-
-        // Wait for all parameters to be updated
-        const updatedParameters = await Promise.all(parameterPromises);
-
-        // Respond with the updated parameters
-        res.status(200).json(updatedParameters);
-    } catch (err) {
-        console.error(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-};
-
-
-// probably not needed -- WILL DELETE
-export const getParametersByType = async (req, res) => {
-    const typeId = req.params.typeId;
-
-    try {
-        const parameters = await Parameter.find({ type_id: typeId });
-        res.status(200).json(parameters);
     } catch (err) {
         console.error(err);
         res.status(500).json({ error: 'Internal Server Error' });

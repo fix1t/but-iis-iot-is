a3c3752261a63987a73577787db394e76eaa76f6
adamgabry <adam.gabrys@seznam.cz>
2023-11-26 21:09:59 +0100

    fixed loading zero devices on systemList

--- src/public/js/device/device-list.js
+++ src/public/js/device/device-list.js
@@ -1,16 +1,19 @@
 fetch('/api/devices/my-devices')
-	.then(response => response.json())
-	.then(devices => {
-		const tableBody = document.getElementById('userDevicesTableBody');
-		devices.forEach(device => {
-			const row = document.createElement('tr');
-			row.innerHTML = `
-				<td>${device.name}</td>
-                <td>${device.description}</td>
-                <td>${device.user_alias}</td>
-				<td><a href="/${device.id}">Detail</a></td>
-            `;
-			tableBody.appendChild(row);
-		});
-	});
-
+    .then(response => response.json())
+    .then(devices => {
+        const tableBody = document.getElementById('userDevicesTableBody');
+        if (devices === null) {
+            tableBody.innerHTML = '<tr><td colspan="4">You have no devices yet</td></tr>';
+        } else {
+            devices.forEach(device => {
+                const row = document.createElement('tr');
+                row.innerHTML = `
+                    <td>${device.name}</td>
+                    <td>${device.description}</td>
+                    <td>${device.user_alias}</td>
+                    <td><a href="/${device.id}">Detail</a></td>
+                `;
+                tableBody.appendChild(row);
+            });
+        }
+    });

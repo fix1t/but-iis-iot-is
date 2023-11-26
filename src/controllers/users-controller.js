f4cace0568b8d194620d07926da871c7f6e9e0c4
Fix1t <gbiel1117@gmail.com>
2023-11-26 21:07:54 +0100

    fix date validity

--- src/controllers/users-controller.js
+++ src/controllers/users-controller.js
@@ -7,39 +7,39 @@ export const registerUser = async (req, res) => {
 	const { username, email, password, confPassword, birth, gender, bio } = req.body;
 	if (isMissingRequiredFields(req.body)) {
 		console.log('missing required fields');
-		res.status(400).json({error: 'Please provide all required fields'});
+		res.status(400).json({ error: 'Please provide all required fields' });
 		return;
 	}
 	if (password !== confPassword) {
 		console.log('passwords do not match');
-		res.status(400).json({error: 'Passwords do not match'});
+		res.status(400).json({ error: 'Passwords do not match' });
 		return;
 	}
 	if (await userAlreadyExists(email, username)) {
 		console.log('user already exists');
-		res.status(400).json({error: 'User with that email or username already exists'});
+		res.status(400).json({ error: 'User with that email or username already exists' });
 		return;
 	}
 	if (!passwordMeetsCriteria(password)) {
 		console.log('password does not meet criteria');
-		res.status(400).json({error: 'Password must contain uppercase,lowercase, number and must be at least 6 characters long.'});
+		res.status(400).json({ error: 'Password must contain uppercase,lowercase, number and must be at least 6 characters long.' });
 		return;
 	}
-	if (birth > Date.now()) {
-		console.log('birth date is not valid');
-		res.status(400).json({error: 'Birth date is not valid'});
+	if (isValidBirth(birth)) {
+		console.log('Birth date is not valid');
+		res.status(400).json({ error: 'Birth date is not valid' });
 		return;
 	}
 	if (!isGenderValid(gender)) {
 		console.log('invalid gender')
-		res.status(400).json({error: 'Gender is not valid'});
+		res.status(400).json({ error: 'Gender is not valid' });
 	}
 
 	try {
-        const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User(username, email, hashedPassword, birth, gender, bio);
+		const hashedPassword = await bcrypt.hash(password, 10);
+		const user = new User(username, email, hashedPassword, birth, gender, bio);
 
-        await user.save();
+		await user.save();
 		await user.getId();
 
 		const token = generateToken(user);
@@ -47,38 +47,38 @@ export const registerUser = async (req, res) => {
 			httpOnly: true, // Important: This helps mitigate the risk of client side script accessing the protected cookie
 			secure: process.env.NODE_ENV === 'production', // Cookies sent only over HTTPS
 			sameSite: 'strict', // CSRF protection
-			maxAge: 3600000 
-		  });
-		console.log('user registered successfully'+user);
-        
-        res.status(200).json({message: 'User registered successfully'});
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({error: 'Internal Server Error'});
-    }
+			maxAge: 3600000
+		});
+		console.log('user registered successfully' + user);
+
+		res.status(200).json({ message: 'User registered successfully' });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 };
 
 export const loginUser = async (req, res) => {
-    const { email, password } = req.body;
+	const { email, password } = req.body;
 
-    try {
+	try {
 		if (!email || !password) {
 			res.status(400).json({ error: 'Please provide an required data email and password.' });
 			return;
 		}
 
-        if (!await emailInUse(email)) {
-            res.status(400).json({ error: 'User not found' });
-            return;
-        }
+		if (!await emailInUse(email)) {
+			res.status(400).json({ error: 'User not found' });
+			return;
+		}
 
 		const user = await User.findByEmail(email);
 
-        const isValidPassword = await bcrypt.compare(password, user.password);
-        if (!isValidPassword) {
-            res.status(400).json({ error: 'Invalid password' });
-            return;
-        }
+		const isValidPassword = await bcrypt.compare(password, user.password);
+		if (!isValidPassword) {
+			res.status(400).json({ error: 'Invalid password' });
+			return;
+		}
 
 		const token = generateToken(user);
 
@@ -86,14 +86,14 @@ export const loginUser = async (req, res) => {
 			httpOnly: true, // Important: This helps mitigate the risk of client side script accessing the protected cookie
 			secure: process.env.NODE_ENV === 'production', // Cookies sent only over HTTPS
 			sameSite: 'strict', // CSRF protection
-			maxAge: 3600000 
-		  });
+			maxAge: 3600000
+		});
 
-        res.status(200).json({ message: 'Logged in successfully' });
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+		res.status(200).json({ message: 'Logged in successfully' });
+	} catch (err) {
+		console.log(err);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 };
 
 export const logoutUser = async (req, res) => {
@@ -106,44 +106,44 @@ export const updateUser = async (req, res) => {
 	let userToBeUpdated;
 
 	// fetch user from db
-	try{
+	try {
 		userToBeUpdated = await User.findById(req.params.id);
-	} catch(err){
+	} catch (err) {
 		console.log(err);
-		return res.status(500).json({error: 'Internal Server Error'});
+		return res.status(500).json({ error: 'Internal Server Error' });
 
 	}
 
-	if(!user || !userToBeUpdated){
-		res.status(400).json({error: 'User not found'});
+	if (!user || !userToBeUpdated) {
+		res.status(400).json({ error: 'User not found' });
 		return;
 	}
 
 	//user can only update his own profile, unless he is an admin
-	if(user.id != userToBeUpdated.id && !user.is_admin){
-		res.status(401).json({error: 'Unauthorized'});
+	if (user.id != userToBeUpdated.id && !user.is_admin) {
+		res.status(401).json({ error: 'Unauthorized' });
 		return;
 	}
 
-	if(userToBeUpdated.username != username && await usernameInUse(username)){
-		res.status(400).json({error: 'Username already in use'});
+	if (userToBeUpdated.username != username && await usernameInUse(username)) {
+		res.status(400).json({ error: 'Username already in use' });
 		return;
 	}
 
-	if(userToBeUpdated.email != email && await emailInUse(email)){
-		res.status(400).json({error: 'Email already in use'});
+	if (userToBeUpdated.email != email && await emailInUse(email)) {
+		res.status(400).json({ error: 'Email already in use' });
 		return;
 	}
 
-	if (birth > Date.now()) {
+	if (isValidBirth(birth)) {
 		console.log('birth date is not valid');
-		res.status(400).json({error: 'Birth date is not valid'});
+		res.status(400).json({ error: 'Birth date is not valid' });
 		return;
 	}
 
 	if (!isGenderValid(gender)) {
 		console.log('invalid gender')
-		res.status(400).json({error: 'Gender is not valid'});
+		res.status(400).json({ error: 'Gender is not valid' });
 	}
 
 	userToBeUpdated.username = username;
@@ -152,12 +152,12 @@ export const updateUser = async (req, res) => {
 	userToBeUpdated.bio = bio;
 	userToBeUpdated.gender = gender;
 
-	try{
+	try {
 		await userToBeUpdated.update();
-		res.status(200).json({message: 'User updated successfully'});
-	} catch(err){
+		res.status(200).json({ message: 'User updated successfully' });
+	} catch (err) {
 		console.log(err);
-		res.status(500).json({error: 'Internal Server Error'});
+		res.status(500).json({ error: 'Internal Server Error' });
 	}
 
 }
@@ -182,54 +182,54 @@ export const deleteUser = async (req, res) => {
 	const idToBeDeleted = req.params.id;
 
 	// user can only delete his own profile, unless he is an admin
-	if(!user.is_admin && user.id != idToBeDeleted){
-		res.status(401).json({error: 'Unauthorized'});
+	if (!user.is_admin && user.id != idToBeDeleted) {
+		res.status(401).json({ error: 'Unauthorized' });
 		return;
-	} else if( user.id == idToBeDeleted){
+	} else if (user.id == idToBeDeleted) {
 		// user is deleting his own profile
 		// delete his session
 		res.clearCookie('auth-token');
 	}
 
-    try {
-        const success = await User.deleteById(idToBeDeleted);
-        if (success) {
-            res.status(200).json({ message: 'User deleted successfully' });
-        } else {
-            res.status(404).json({ error: 'User not found' });
-        }
-    } catch (error) {
-        console.error('Error executing query:', error.stack);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+	try {
+		const success = await User.deleteById(idToBeDeleted);
+		if (success) {
+			res.status(200).json({ message: 'User deleted successfully' });
+		} else {
+			res.status(404).json({ error: 'User not found' });
+		}
+	} catch (error) {
+		console.error('Error executing query:', error.stack);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 };
 
 export const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.findAll();
+	try {
+		const users = await User.findAll();
 		// TODO: leave out id and password
 		const filteredUsers = users.map(user => {
-		return {
-			id: user.id,
-			username: user.username,
-			email: user.email,
-			bio: user.bio,
-			birth: user.birth,
-			gender: user.gender,
-			created: user.created
-		};
+			return {
+				id: user.id,
+				username: user.username,
+				email: user.email,
+				bio: user.bio,
+				birth: user.birth,
+				gender: user.gender,
+				created: user.created
+			};
 		});
 		res.json(filteredUsers);
-    } catch (error) {
-        console.error('Error executing query:', error.stack);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+	} catch (error) {
+		console.error('Error executing query:', error.stack);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 };
 
 export const getUser = async (req, res) => {
-    try {
-        const user = await User.findById(req.params.id);
-        if (user) {
+	try {
+		const user = await User.findById(req.params.id);
+		if (user) {
 			const data = {
 				username: user.username,
 				email: user.email,
@@ -237,14 +237,14 @@ export const getUser = async (req, res) => {
 				birth: user.birth,
 				gender: user.gender
 			}
-            res.json(data);
-        } else {
-            res.status(404).json({ message: 'User not found' });
-        }
-    } catch (error) {
-        console.error('Error executing query:', error.stack);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
+			res.json(data);
+		} else {
+			res.status(404).json({ message: 'User not found' });
+		}
+	} catch (error) {
+		console.error('Error executing query:', error.stack);
+		res.status(500).json({ error: 'Internal Server Error' });
+	}
 };
 
 function isMissingRequiredFields(user) {
@@ -268,11 +268,16 @@ async function emailInUse(email) {
 
 function passwordMeetsCriteria(password) {
 	var criteriaRegExp = new RegExp(
-        "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,}$"
-    );
-    return criteriaRegExp.test(password);
+		"^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{6,}$"
+	);
+	return criteriaRegExp.test(password);
 }
 
-function isGenderValid(gender){
+function isGenderValid(gender) {
 	return gender == 'male' || gender == 'female' || gender == 'other';
+}
+
+function isValidBirth(date) {
+	const birthDate = new Date(date);
+	return isNaN(birthDate) || birthDate > new Date();
 }
